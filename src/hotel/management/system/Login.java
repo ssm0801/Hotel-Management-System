a878f2ed32ceb8642869940ca4493f03c6cf92dc
Sudhanshu Motewar <sudhanshumotewar2001@gmail.com>
2021-03-22 22:59:43 +0530

    Wrote code for username and password labels, fields, and login and cancel button and gave some properties

--- src/hotel/management/system/Login.java
+++ src/hotel/management/system/Login.java
@@ -17,6 +17,40 @@ public class Login extends JFrame{
         login_image_label.setBounds(150,50,200,200);
         add(login_image_label);
 
+        // Username label and its size and location
+        JLabel un_label = new JLabel("Username : ");
+        un_label.setBounds(140,300,100,30);
+        add(un_label);
+
+        // Password label and its size and location
+        JLabel psd_label = new JLabel("Password : ");
+        psd_label.setBounds(140,350,100,30);
+        add(psd_label);
+
+        // Username field and its size and location
+        JTextField un_field = new JTextField();
+        un_field.setBounds(210,300,150,30);
+        add(un_field);
+
+        // Password field and its size and location
+        JPasswordField psd_field = new JPasswordField();
+        psd_field.setBounds(210,350,150,30);
+        add(psd_field);
+
+        // Login button and its properties, size and location
+        JButton login_button = new JButton("Login");
+        login_button.setBounds(150,400,90,30);
+        login_button.setBackground(Color.BLACK);
+        login_button.setForeground(Color.WHITE);
+        add(login_button);
+
+        // Cancel button and its properties, size and location
+        JButton cancel_button = new JButton("Cancel");
+        cancel_button.setBounds(250,400,90,30);
+        cancel_button.setBackground(Color.BLACK);
+        cancel_button.setForeground(Color.WHITE);
+        add(cancel_button);
+
 
         setLayout(null);
         setVisible(true);
