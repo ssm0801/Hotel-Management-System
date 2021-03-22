fc5a12581373d4bcd46ac2cd4daba8389d48133a
Sudhanshu Motewar <sudhanshumotewar2001@gmail.com>
2021-03-22 17:09:15 +0530

    written code for size and location of bounding box

--- src/hotel/management/system/HotelManagementSystem.java
+++ src/hotel/management/system/HotelManagementSystem.java
@@ -1,8 +1,17 @@
 package hotel.management.system;
+import javax.swing.*;
+import java.awt.*;
 
-public class HotelManagementSystem {
+public class HotelManagementSystem extends JFrame {
+
+    HotelManagementSystem(){
+
+        // size and location of bounding box
+        setBounds(450,225,640,427);
 
 
+
+    }
 
     public static void main(String[] args) {
         HotelManagementSystem hotel1 = new HotelManagementSystem();
