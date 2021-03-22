5d0546085ececa616b7c1f2c9b9e4da4df5b7f68
ssm0801 <sudhanshumotewar2001@gmail.com>
2021-03-22 16:26:55 +0530

    Created HotelManagementSystem.java

---
+++ src/hotel/management/system/HotelManagementSystem.java
@@ -0,0 +1,11 @@
+package hotel.management.system;
+
+public class HotelManagementSystem {
+
+
+
+    public static void main(String[] args) {
+        HotelManagementSystem hotel1 = new HotelManagementSystem();
+    }
+
+}
