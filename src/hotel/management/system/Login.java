559d41f4b07b1b9a06546a78795a2fb26d7e0616
Sudhanshu Motewar <sudhanshumotewar2001@gmail.com>
2021-03-22 22:58:42 +0530

    Wrote code for inserting login.jpeg and changing its dimension

--- src/hotel/management/system/Login.java
+++ src/hotel/management/system/Login.java
@@ -9,6 +9,14 @@ public class Login extends JFrame{
         // size and location of bounding box
         setBounds(500,130,500,600);
 
+        // Inserted image and changed its dimension and gave location in dialog box
+        ImageIcon login_image = new ImageIcon(ClassLoader.getSystemResource("images/login.jpeg"));
+        Image img = login_image.getImage().getScaledInstance(200,200, Image.SCALE_DEFAULT);
+        ImageIcon new_login_image = new ImageIcon(img);
+        JLabel login_image_label = new JLabel(new_login_image);
+        login_image_label.setBounds(150,50,200,200);
+        add(login_image_label);
+
 
         setLayout(null);
         setVisible(true);
