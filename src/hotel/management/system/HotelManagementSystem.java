097c6791ddf527eecd7485303f57db276f2296c9
Sudhanshu Motewar <sudhanshumotewar2001@gmail.com>
2021-03-22 17:10:03 +0530

    written code for image path, its label, size and location

--- src/hotel/management/system/HotelManagementSystem.java
+++ src/hotel/management/system/HotelManagementSystem.java
@@ -9,6 +9,13 @@ public class HotelManagementSystem extends JFrame {
         // size and location of bounding box
         setBounds(450,225,640,427);
 
+        // image path and label
+        ImageIcon main_image = new ImageIcon(ClassLoader.getSystemResource("images/hotel1.jpg"));
+        JLabel main_image_lable = new JLabel(main_image);
+
+        // image size and location inside bounding box
+        main_image_lable.setBounds(0,0,640,427);
+        add(main_image_lable);
 
 
     }
