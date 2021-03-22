6474f97de0a6cb1205e09082d7436965a91edb92
Sudhanshu Motewar <sudhanshumotewar2001@gmail.com>
2021-03-22 22:56:54 +0530

    Created Login.java and bounded dialog box

---
+++ src/hotel/management/system/Login.java
@@ -0,0 +1,21 @@
+package hotel.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Login extends JFrame{
+
+    Login(){
+        // size and location of bounding box
+        setBounds(500,130,500,600);
+
+
+        setLayout(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        Login login = new Login();
+    }
+
+}
