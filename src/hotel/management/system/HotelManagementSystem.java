d9ebe1e46079edfb4d55103ce8e88b6e9f60fcbd
Sudhanshu Motewar <sudhanshumotewar2001@gmail.com>
2021-03-22 17:30:12 +0530

    written code for button

--- src/hotel/management/system/HotelManagementSystem.java
+++ src/hotel/management/system/HotelManagementSystem.java
@@ -5,19 +5,18 @@ import java.awt.*;
 public class HotelManagementSystem extends JFrame {
 
     HotelManagementSystem(){
-
         // size and location of bounding box
         setBounds(450,225,640,427);
 
         // image path and label
-        ImageIcon main_image = new ImageIcon(ClassLoader.getSystemResource("images/hotel1.jpg"));
+        ImageIcon main_image = new ImageIcon(ClassLoader.getSystemResource("images/hotel.jpg"));
         JLabel main_image_lable = new JLabel(main_image);
 
         // image size and location inside bounding box
         main_image_lable.setBounds(0,0,640,427);
         add(main_image_lable);
 
-        // heading and its properties
+        // heading and its properties (bounds, foreground color, font)
         JLabel heading = new JLabel("Hotel Management System");
         heading.setBounds(120,0,400,70);
         heading.setForeground(Color.BLACK);
@@ -26,6 +25,14 @@ public class HotelManagementSystem extends JFrame {
 
         setLayout(null);
         setVisible(true);
+
+        // Button and its properties (bounds, background color, foreground color, font)
+        JButton button = new JButton("CONTINUE");
+        button.setBounds(240,300,150,50);
+        button.setBackground(Color.BLACK);
+        button.setForeground(Color.WHITE);
+        button.setFont(new Font("times new roman", Font.BOLD, 20));
+        main_image_lable.add(button);
 
         // heading blinker function
         while(true){
@@ -39,7 +46,6 @@ public class HotelManagementSystem extends JFrame {
             try{ Thread.sleep(300); }
             catch (Exception e){ }
         }
-
     }
 
     public static void main(String[] args) {
