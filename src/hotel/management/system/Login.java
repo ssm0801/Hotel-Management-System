4d50e997feca6ac15dc961b926749695b4a30517
Sudhanshu Motewar <sudhanshumotewar2001@gmail.com>
2021-03-24 21:56:17 +0530

    Updated Login.java

--- src/hotel/management/system/Login.java
+++ src/hotel/management/system/Login.java
@@ -8,42 +8,40 @@ import java.sql.*;
 public class Login extends JFrame implements ActionListener{
 
     // all objects declared globally in class to use in methods
-    ImageIcon login_image, new_login_image;
-    Image img;
-    JLabel login_image_label, un_label, psd_label;
+    JButton login_button, cancel_button;
     JTextField un_field;
     JPasswordField psd_field;
-    JButton login_button, cancel_button;
 
     public Login(){
         // size and location of bounding box
         setBounds(500,130,500,600);
+        setLayout(null);
 
         // Inserted image and changed its dimension and gave location in dialog box
-        login_image = new ImageIcon(ClassLoader.getSystemResource("images/login.jpeg"));
-        img = login_image.getImage().getScaledInstance(200,200, Image.SCALE_DEFAULT);
-        new_login_image = new ImageIcon(img);
-        login_image_label = new JLabel(new_login_image);
+        ImageIcon login_image = new ImageIcon(ClassLoader.getSystemResource("images/login.jpeg"));
+        Image img = login_image.getImage().getScaledInstance(200,200, Image.SCALE_DEFAULT);
+        ImageIcon new_login_image = new ImageIcon(img);
+        JLabel login_image_label = new JLabel(new_login_image);
         login_image_label.setBounds(150,50,200,200);
         add(login_image_label);
 
         // Username label and its size and location
-        un_label = new JLabel("Username : ");
+        JLabel un_label = new JLabel("Username : ");
         un_label.setBounds(140,300,100,30);
         add(un_label);
 
         // Password label and its size and location
-        psd_label = new JLabel("Password : ");
+        JLabel psd_label = new JLabel("Password : ");
         psd_label.setBounds(140,350,100,30);
         add(psd_label);
 
         // Username field and its size and location
-        un_field = new JTextField();
+        JTextField un_field = new JTextField();
         un_field.setBounds(210,300,150,30);
         add(un_field);
 
         // Password field and its size and location
-        psd_field = new JPasswordField();
+        JPasswordField psd_field = new JPasswordField();
         psd_field.setBounds(210,350,150,30);
         add(psd_field);
 
@@ -66,38 +64,38 @@ public class Login extends JFrame implements ActionListener{
         // Change dialog box background color
         getContentPane().setBackground(Color.WHITE);
 
-        setLayout(null);
         setVisible(true);
     }
 
     // action function for login and cancel buttons
     public void actionPerformed(ActionEvent action){
         if (action.getSource() == login_button){
-            String username = un_field.getText();
-            String password = psd_field.getText();
-            DatabaseConnection connect = new DatabaseConnection();
-            String query = "select * from login where username = '"+username+"' and password = '"+password+"'";
-
             try {
+                String username = un_field.getText();
+                String password = psd_field.getText();
+                DatabaseConnection connect = new DatabaseConnection();
+                String query = "select * from login where username = '"+username+"' and password = '"+password+"'";
                 ResultSet result = connect.statement.executeQuery(query);
                 if (result.next()){
-                    this.setVisible(false);
-                    System.exit(0);
+                    new Dashboard().setVisible(true);
                 }else {
                     JOptionPane.showMessageDialog(null, "Invalid Login !!!");
                 }
+                setVisible(false);
             }catch (Exception e){
                 System.out.println(e);
             }
         }
         // exit the application
         else if (action.getSource() == cancel_button){
-            System.exit(0);
+//            System.exit(0);
+            setVisible(false);
+            new HotelManagementSystem().setVisible(true);
         }
     }
 
     public static void main(String[] args) {
-        Login login = new Login();
+        new Login();
     }
 
 }
