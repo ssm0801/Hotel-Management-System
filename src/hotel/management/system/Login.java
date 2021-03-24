1e7aeae822a49bf3d93ee64d19d62f4b066c15b1
Sudhanshu Motewar <sudhanshumotewar2001@gmail.com>
2021-03-24 17:24:12 +0530

    Updated Login.java

--- src/hotel/management/system/Login.java
+++ src/hotel/management/system/Login.java
@@ -3,33 +3,37 @@ package hotel.management.system;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.sql.*;
 
 public class Login extends JFrame implements ActionListener{
 
-    // field and button declared globally in class to use in methods
+    // all objects declared globally in class to use in methods
+    ImageIcon login_image, new_login_image;
+    Image img;
+    JLabel login_image_label, un_label, psd_label;
     JTextField un_field;
     JPasswordField psd_field;
     JButton login_button, cancel_button;
 
-    Login(){
+    public Login(){
         // size and location of bounding box
         setBounds(500,130,500,600);
 
         // Inserted image and changed its dimension and gave location in dialog box
-        ImageIcon login_image = new ImageIcon(ClassLoader.getSystemResource("images/login.jpeg"));
-        Image img = login_image.getImage().getScaledInstance(200,200, Image.SCALE_DEFAULT);
-        ImageIcon new_login_image = new ImageIcon(img);
-        JLabel login_image_label = new JLabel(new_login_image);
+        login_image = new ImageIcon(ClassLoader.getSystemResource("images/login.jpeg"));
+        img = login_image.getImage().getScaledInstance(200,200, Image.SCALE_DEFAULT);
+        new_login_image = new ImageIcon(img);
+        login_image_label = new JLabel(new_login_image);
         login_image_label.setBounds(150,50,200,200);
         add(login_image_label);
 
         // Username label and its size and location
-        JLabel un_label = new JLabel("Username : ");
+        un_label = new JLabel("Username : ");
         un_label.setBounds(140,300,100,30);
         add(un_label);
 
         // Password label and its size and location
-        JLabel psd_label = new JLabel("Password : ");
+        psd_label = new JLabel("Password : ");
         psd_label.setBounds(140,350,100,30);
         add(psd_label);
 
@@ -39,7 +43,7 @@ public class Login extends JFrame implements ActionListener{
         add(un_field);
 
         // Password field and its size and location
-         psd_field = new JPasswordField();
+        psd_field = new JPasswordField();
         psd_field.setBounds(210,350,150,30);
         add(psd_field);
 
@@ -66,10 +70,28 @@ public class Login extends JFrame implements ActionListener{
         setVisible(true);
     }
 
+    // action function for login and cancel buttons
     public void actionPerformed(ActionEvent action){
         if (action.getSource() == login_button){
+            String username = un_field.getText();
+            String password = psd_field.getText();
+            DatabaseConnection connect = new DatabaseConnection();
+            String query = "select * from login where username = '"+username+"' and password = '"+password+"'";
 
-        }else if (action.getSource() == cancel_button){
+            try {
+                ResultSet result = connect.statement.executeQuery(query);
+                if (result.next()){
+                    this.setVisible(false);
+                    System.exit(0);
+                }else {
+                    JOptionPane.showMessageDialog(null, "Invalid Login !!!");
+                }
+            }catch (Exception e){
+                System.out.println(e);
+            }
+        }
+        // exit the application
+        else if (action.getSource() == cancel_button){
             System.exit(0);
         }
     }
