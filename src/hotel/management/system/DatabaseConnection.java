ba00e665e1297eb6622d0170a6ea19d89e6101c9
Sudhanshu Motewar <sudhanshumotewar2001@gmail.com>
2021-03-24 17:23:03 +0530

    Create DatabaseConnection.java to connect with database and wrote code

--- src/hotel/management/system/DatabaseConnection.java
+++ src/hotel/management/system/DatabaseConnection.java
@@ -1,5 +1,27 @@
 package hotel.management.system;
 
+import java.sql.*;
+
 public class DatabaseConnection {
+
+    // all objects declared globally in class to use in methods
+    Connection connection;
+    Statement statement;
+
+    public DatabaseConnection(){
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql:///hms", "root", "toor");
+            statement = connection.createStatement();
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 
 }
