743c8dbc473dbe6c72b22aeb6fe5ab0a642ede76
Sudhanshu Motewar <sudhanshumotewar2001@gmail.com>
2021-03-24 06:29:16 +0530

    Made all objects global inside class

--- src/hotel/management/system/HotelManagementSystem.java
+++ src/hotel/management/system/HotelManagementSystem.java
@@ -15,27 +15,31 @@ import java.awt.event.*;
 
 public class HotelManagementSystem extends JFrame implements ActionListener{
 
+    ImageIcon main_image;
+    JLabel main_image_lable, heading;
+    JButton button;
+
     HotelManagementSystem(){
         // size and location of bounding box
         setBounds(450,225,640,427);
 
         // image path and label
-        ImageIcon main_image = new ImageIcon(ClassLoader.getSystemResource("images/hotel.jpg"));
-        JLabel main_image_lable = new JLabel(main_image);
+        main_image = new ImageIcon(ClassLoader.getSystemResource("images/hotel.jpg"));
+        main_image_lable = new JLabel(main_image);
 
         // image size and location inside bounding box
         main_image_lable.setBounds(0,0,640,427);
         add(main_image_lable);
 
         // heading and its properties (bounds, foreground color, font)
-        JLabel heading = new JLabel("Hotel Management System");
+        heading = new JLabel("Hotel Management System");
         heading.setBounds(120,0,400,70);
         heading.setForeground(Color.BLACK);
         heading.setFont(new Font("times new roman", Font.ITALIC, 35));
         main_image_lable.add(heading);
 
         // Button and its properties (bounds, background color, foreground color, font)
-        JButton button = new JButton("CONTINUE");
+        button = new JButton("CONTINUE");
         button.setBounds(243,300,150,50);
         button.setBackground(Color.BLACK);
         button.setForeground(Color.WHITE);
